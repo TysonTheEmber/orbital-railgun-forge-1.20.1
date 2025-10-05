dd5cf31b3d7b71807718b6c8cd8dc5018315238c
TysonTheEmber <tysonpeterson2007@gmail.com>
2025-10-05 16:59:34 -0600

    fix

--- src/main/java/net/tysontheember/orbitalrailgun/client/render/OrbRenderTypes.java
+++ src/main/java/net/tysontheember/orbitalrailgun/client/render/OrbRenderTypes.java
@@ -1,50 +1,43 @@
 package net.tysontheember.orbitalrailgun.client.render;
 
+import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.DefaultVertexFormat;
 import com.mojang.blaze3d.vertex.VertexFormat;
 import net.minecraft.client.renderer.RenderStateShard;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.resources.ResourceLocation;
+import org.lwjgl.opengl.GL11;
 
 public final class OrbRenderTypes {
-    private OrbRenderTypes() {
-    }
-
-    public static RenderType beam(ResourceLocation texture) {
+    private static RenderType make(ResourceLocation tex, RenderStateShard.ShaderStateShard shader) {
         RenderType.CompositeState state = RenderType.CompositeState.builder()
-                .setShaderState(new RenderStateShard.ShaderStateShard(() -> ClientRender.ORB_BEAM_SHADER))
-                .setTextureState(new RenderStateShard.TextureStateShard(texture, false, false))
-                .setTransparencyState(RenderStateShard.TRANSLUCENT_TRANSPARENCY)
-                .setDepthTestState(RenderStateShard.LEQUAL_DEPTH_TEST)
-                .setCullState(RenderStateShard.NO_CULL)
+                .setShaderState(shader)
+                .setTextureState(new RenderStateShard.TextureStateShard(tex, false, false))
+                .setTransparencyState(new RenderStateShard.TransparencyStateShard(
+                        "orb_translucent",
+                        () -> { RenderSystem.enableBlend(); RenderSystem.defaultBlendFunc(); },
+                        RenderSystem::disableBlend
+                ))
+                .setDepthTestState(new RenderStateShard.DepthTestStateShard("lequal", GL11.GL_LEQUAL))
+                .setCullState(new RenderStateShard.CullStateShard(false))
                 .createCompositeState(false);
+
         return RenderType.create(
-                "orbital_railgun_beam",
+                "orb_rt",
                 DefaultVertexFormat.POSITION_COLOR_TEX,
                 VertexFormat.Mode.QUADS,
-                256,
-                false,
-                true,
+                256,      // buffer size
+                false,    // affectsCrumbling
+                true,     // sortOnUpload (good for translucency)
                 state
         );
     }
 
-    public static RenderType marker(ResourceLocation texture) {
-        RenderType.CompositeState state = RenderType.CompositeState.builder()
-                .setShaderState(new RenderStateShard.ShaderStateShard(() -> ClientRender.ORB_MARKER_SHADER))
-                .setTextureState(new RenderStateShard.TextureStateShard(texture, false, false))
-                .setTransparencyState(RenderStateShard.TRANSLUCENT_TRANSPARENCY)
-                .setDepthTestState(RenderStateShard.LEQUAL_DEPTH_TEST)
-                .setCullState(RenderStateShard.NO_CULL)
-                .createCompositeState(false);
-        return RenderType.create(
-                "orbital_railgun_marker",
-                DefaultVertexFormat.POSITION_COLOR_TEX,
-                VertexFormat.Mode.QUADS,
-                256,
-                false,
-                true,
-                state
-        );
+    public static RenderType beam(ResourceLocation tex) {
+        return make(tex, new RenderStateShard.ShaderStateShard(() -> ClientRender.ORB_BEAM_SHADER));
+    }
+
+    public static RenderType marker(ResourceLocation tex) {
+        return make(tex, new RenderStateShard.ShaderStateShard(() -> ClientRender.ORB_MARKER_SHADER));
     }
 }
--- src/main/java/net/tysontheember/orbitalrailgun/client/render/ShaderRegistration.java
+++ src/main/java/net/tysontheember/orbitalrailgun/client/render/ShaderRegistration.java
@@ -1,7 +1,7 @@
 package net.tysontheember.orbitalrailgun.client.render;
 
 import net.minecraft.client.renderer.ShaderInstance;
-import net.minecraft.client.renderer.vertex.DefaultVertexFormat;
+import com.mojang.blaze3d.vertex.DefaultVertexFormat;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.RegisterShadersEvent;
@@ -19,14 +19,14 @@ public final class ShaderRegistration {
     public static void onRegisterShaders(RegisterShadersEvent event) throws IOException {
         ShaderInstance beam = new ShaderInstance(
                 event.getResourceProvider(),
-                new ResourceLocation("orbital_railgun", "core/orb_beam"),
+                new ResourceLocation("orbital_railgun", "orb_beam"),
                 DefaultVertexFormat.POSITION_COLOR_TEX
         );
         event.registerShader(beam, shader -> ClientRender.ORB_BEAM_SHADER = shader);
 
         ShaderInstance marker = new ShaderInstance(
                 event.getResourceProvider(),
-                new ResourceLocation("orbital_railgun", "core/orb_marker"),
+                new ResourceLocation("orbital_railgun", "orb_marker"),
                 DefaultVertexFormat.POSITION_COLOR_TEX
         );
         event.registerShader(marker, shader -> ClientRender.ORB_MARKER_SHADER = shader);
